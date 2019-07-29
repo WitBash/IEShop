e2de1f3436bbc77b8d354bf73f277dbb360f085d
WitBash <yr-83p@mail.ru>
2019-07-30 00:46:11 +0300

    fixed mistakes in app

--- ieshop-market-ui/src/main/java/com/boshko/ieshop/security/SecurityConfig.java
+++ ieshop-market-ui/src/main/java/com/boshko/ieshop/security/SecurityConfig.java
@@ -42,9 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeRequests()
-                //.anyRequest().permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .and()
                 .formLogin()
